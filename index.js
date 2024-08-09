1c410f51348cb169db7ae67b54aba643fe61d641
Hinjikani <72244251+Hinjikani@users.noreply.github.com>
2024-08-10 06:24:54 +0700

    made get human choice & playround

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function computerChoice() {
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*100) + 1;
     if (randomNumber % 2 == 0) {
         let randomNumber = Math.floor(Math.random()*100) + 1;
@@ -12,4 +12,51 @@ function computerChoice() {
     }
 }
 
-console.log(computerChoice())
+function getHumanChoice() {
+    let humanChoice = prompt("what is your move?")
+    return humanChoice.toLowerCase()
+}
+
+let humanScore = 0
+let computerScore = 0
+let tie = 0
+
+function playround() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    //=================TIE SCENARIO=================
+    if (computerChoice === humanChoice) {
+        tie++;
+        alert(`It's a tie, both of you choose ${humanChoice}\n\n<--- Current Score --->\nYour score: ${humanScore}\nOpponent score: ${computerScore}\nTie: ${tie}`);
+    }
+    
+    //=================WIN SCENARIO=================
+    else if (
+        humanChoice === "scissors" && computerChoice === "paper" ||
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock"
+    ) {
+        humanScore++;
+        alert(`You win!!!\nYou choose ${humanChoice} and your opponent choose ${computerChoice}\n\n<--- Current Score --->\nYour score: ${humanScore}\nOpponent score: ${computerScore}\nTie: ${tie}`)
+    }
+    
+    //=================LOSE SCENARIO=================
+    else if (
+        humanChoice === "scissors" && computerChoice === "rock" ||
+        humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "scissors"
+    )
+    {
+        computerScore++;
+        alert(`You lose!!!\nYou choose ${humanChoice} and your opponent choose ${computerChoice}\n\n<--- Current Score --->\nYour Score: ${humanScore}\nOpponent Score: ${computerScore}\nTie: ${tie}`)
+    }
+        }
+    
+playround()
+playround()
+playround()
+playround()
+playround()
+
+alert(`Final Score\nYour score: ${humanScore}\nOpponent score: ${computerScore}\nTie: ${tie}`)
